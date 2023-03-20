a38b619860b12a8405e06184217044c001cf58e1
Ben Coladarci <bencoladarci@uri.edu>
2023-03-20 18:52:42 -0400

    Update helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,9 @@
-userName = input ("Hello! Please enter you name: ")
-print ("Hello", userName, "nice to meet you")
+
+userName = input("Hello! Please enter your facebook username! ")
+
+userPassword = input("Hello! Please enter your facebook password! ")
+
+if(userName.lower() == "ben" and userPassword == "iLoveCoding!"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
